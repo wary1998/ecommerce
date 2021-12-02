e42c7f9c92ae9d04bb927fe710a824ce339edc4d
Wary Hossain Rabby <59994865+wary1998@users.noreply.github.com>
2021-12-02 20:07:24 +0600

    upload

---
+++ ecommerce/includes/banner_promotion.php
@@ -0,0 +1,42 @@
+<div class="banner-promotion-01 xs-margin-top-50px sm-margin-top-70px">
+                <div class="biolife-banner promotion3 biolife-banner__promotion3 green-style">
+                    <div class="banner-contain">
+                        <div class="media">
+                            <div class="img-moving position-1">
+                                <img src="assets/images/home-03/img-moving-pst-1-geen.png" width="149" height="139" alt="img msv">
+                            </div>
+                        </div>
+                        <div class="text-content">
+                            <div class="container text-wrap">
+                                <span class="first-line">Healthy Food</span>
+                                <b class="second-line">Vegetable Always fresh</b>
+                                <p class="third-line">Food Heaven Made Easy sounds like the name of an amazingly delicious food delivery service, but don't be fooled...</p>
+                                <div class="product-detail">
+                                    <p class="txt-price"><span>Only:</span>$8.00</p>
+                                    <a href="#" class="btn add-to-cart-btn">add to cart</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!--Block 05: Banner promotion 02-->
+            <div class="banner-promotion-02 z-index-20">
+                <div class="biolife-banner promotion2 biolife-banner__promotion2 advance">
+                    <div class="banner-contain">
+                        <div class="container">
+                            <div class="media"></div>
+                            <div class="text-content">
+                                <b class="first-line">Food Heaven Made</b>
+                                <span class="second-line">Easy <i>Healthy, Happy Life</i></span>
+                                <p class="third-line">Food Heaven Made Easy sounds like the name of an amazingly delicious food delivery service, but don't be fooled. The blog is actually a compilation of recipes, cooking videos, and nutrition tips.</p>
+                                <p class="buttons">
+                                    <a href="#" class="btn btn-bold">Read More</a>
+                                    <a href="#" class="btn btn-thin">View Menu Now</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
